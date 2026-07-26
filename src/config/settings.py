99262a453e7e350056bb89f86eb6580edc5c1d47
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-07-26 15:34:02 +0530

    feat(config): implement central configuration class for api keys and models

---
+++ src/config/settings.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config():
+
+     GROQ_API_KEY=os.environ.get("GROQ_API_KEY","")
+
+     GOOGLE_API_KEY=os.environ.get("GOOGLE_API_KEY","")
+
+     MISTRAL_API_KEY=os.environ.get("MISTRAL_API_KEY","")
+
+     TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY","")
+
+     GROQ_MODEL=os.environ.get("GROQ_MODEL","")
+
+     GOOGLE_MODEL=os.environ.get("GOOGLE_MODEL","")
+
+     MISTRAL_MODEL=os.environ.get("MISTRAL_MODEL","")
+
+
