21a2df540b8ddb155fbe81c770c71079ed18af01
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-07-29 17:53:40 +0530

    refactor(tools): update tool module exports

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -3,5 +3,5 @@ from .image_generator_tool import generate_image
 
 from .web_scraper_tool import scrape_website
 
-from .web_search_tool import duckduckgo_search, tavily_search
+from .web_search_tool import tavily_search
 
